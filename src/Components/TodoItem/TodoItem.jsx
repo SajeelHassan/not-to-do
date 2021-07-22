360a116e17cfe1d78f0bd420752fceb8acaa9bc4
Sajeel Hassan <sajeel.hassan@tintash.com>
2021-07-22 16:30:44 +0500

    Converted my pure CSS implementation into Components

--- src/Components/TodoItem/TodoItem.jsx
+++ src/Components/TodoItem/TodoItem.jsx
@@ -1,20 +1,27 @@
 import React from "react";
 import PropTypes from "prop-types";
 import * as styles from "./TodoItem.module.css";
+import removeIcon from "../../assets/images/cross.png";
 const TodoItem = (props) => {
   return (
     <>
-      <div className={`${styles["todo-item"]}`}>
-        <input type="checkbox" name="todo-1" id={`"todo-"${props.key}`} />
-
+      <li
+        className={`${styles["todo-item"]}`}
+        id={`"todo-"${props.key}`}
+        key={props.key}
+      >
+        <input type="checkbox" />
         <p>{props.title}</p>
         <span>
-          <img src="./images/cross.png" alt="" />
+          <img src={removeIcon} />
         </span>
-      </div>
+      </li>
     </>
   );
 };
 
-TodoItem.PropTypes = {};
+TodoItem.propTypes = {
+  key: PropTypes.string,
+  title: PropTypes.string,
+};
 export default TodoItem;
--- src/Components/TodoItems/TodoItems.jsx
+++ src/Components/TodoItems/TodoItems.jsx
@@ -1,24 +1,32 @@
 import React from "react";
+import PropTypes from "prop-types";
 import TodoItem from "../TodoItem/TodoItem.jsx";
 import * as styles from "./TodoItems.module.css";
-const TodoItems = () => {
+const TodoItems = ({ storedItems }) => {
   return (
     <div id="main" className={styles.main}>
-      <div className={`${styles["todo-items"]} ${styles.shadow}`}>
-        <TodoItem key="99" title="Hello" />;
+      <ul className={`${styles["todo-items"]} ${styles.shadow}`}>
+        {storedItems.map((item) => (
+          <TodoItem key={item.key} title={item.title} />
+        ))}
         <div className={styles["todo-info"]}>
           <p className={styles["items-left"]}>5 items left</p>
           <p className={styles["clear-completed"]}>Clear Completed</p>
         </div>
-      </div>
+      </ul>
       <div
         className={`${styles["todo-info"]} ${styles["todo-info-2"]} ${styles.show_items}`}
       >
-        <button id="show_items__all">All</button>
+        <button id="show_items__all" focus="true">
+          All
+        </button>
         <button id="show_items__active">Active</button>
         <button id="show_items__completed">Completed</button>
       </div>
     </div>
   );
+};
+TodoItems.propTypes = {
+  storedItems: PropTypes.array,
 };
 export default TodoItems;
