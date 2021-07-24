0228bf9da60c79e1638977e1dcdb448d624ef1fe
Sajeel Hassan <sajeel.hassan@tintash.com>
2021-07-24 19:53:31 +0500

    wip: Sorted item listing via links

--- src/Components/TodoItems/TodoItems.jsx
+++ src/Components/TodoItems/TodoItems.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import PropTypes from "prop-types";
 import TodoItem from "../TodoItem/TodoItem.jsx";
 import * as styles from "./TodoItems.module.css";
@@ -8,15 +8,43 @@ const TodoItems = ({
   removeTodoHandler,
   removeCompletedHandler,
 }) => {
+  const [todoItems, setTodoItems] = useState([]);
+  const [showAll, setShowAll] = useState([]);
+  const [showActive, setShowActive] = useState([]);
+  const [showCompleted, setShowCompleted] = useState([]);
+  useEffect(() => {
+    setTodoItems(storedItems);
+  }, [storedItems]);
+  useEffect(() => {
+    setTodoItems(showAll);
+  }, [showAll]);
+  useEffect(() => {
+    setTodoItems(showActive);
+  }, [showActive]);
+  useEffect(() => {
+    setTodoItems(showCompleted);
+  }, [showCompleted]);
+  const ShowAll = () => {
+    setShowAll(storedItems);
+  };
+  const ShowCompleted = () => {
+    let items = [...storedItems];
+    setShowCompleted(items.filter((todo) => todo.active === false));
+  };
+  const ShowActive = () => {
+    let items = [...storedItems];
+    setShowActive(items.filter((todo) => todo.active === true));
+  };
+
   return (
     <div id="main" className={styles.main}>
       <ul className={`${styles["todo-items"]}`}>
-        {!storedItems.length ? (
+        {!todoItems.length ? (
           <li className={`${styles["nothing"]}`} id="nothing">
             <h3>Add your not TODO!</h3>
           </li>
         ) : (
-          storedItems.map((item) => (
+          todoItems.map((item) => (
             <TodoItem
               title={item.title}
               active={item.active}
@@ -28,7 +56,7 @@ const TodoItems = ({
         )}
       </ul>
       <div className={`${styles["todo-info"]} ${styles["d-flex"]}`}>
-        <p className={styles["items-left"]}>{storedItems.length} items left</p>
+        <p className={styles["items-left"]}>Total items {todoItems.length}</p>
         <p
           className={styles["clear-completed"]}
           onClick={removeCompletedHandler}
@@ -39,9 +67,15 @@ const TodoItems = ({
       <div
         className={`${styles["todo-info"]} ${styles["todo-info-2"]} ${styles.show_items}`}
       >
-        <button id="show_items__all">All</button>
-        <button id="show_items__active">Active</button>
-        <button id="show_items__completed">Completed</button>
+        <button id="show_items__all" onClick={ShowAll}>
+          All
+        </button>
+        <button id="show_items__active" onClick={ShowActive}>
+          Active
+        </button>
+        <button id="show_items__completed" onClick={ShowCompleted}>
+          Completed
+        </button>
       </div>
     </div>
   );
